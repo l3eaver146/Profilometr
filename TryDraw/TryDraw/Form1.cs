e83b202abd4308c22fe79a5f106efae5094ea47a
l3eaver146 <l3eaver146@mail.ru>
2020-09-10 20:59:37 +0300

    Add ZegGraph

---
+++ TryDraw/TryDraw/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace TryDraw
+{
+    public partial class Form1 : Form
+    {
+        GraphPane graphPane;
+        public Form1()
+        {
+            InitializeComponent();
+            graphPane = zedGraphControl1.GraphPane;
+            draw();
+        }
+        private void draw()
+        {
+            graphPane.Title.Text = "Неровность";
+            graphPane.XAxis.Title.Text = "oX";
+            graphPane.YAxis.Title.Text = "oY";
+            ReadData obj = new ReadData();
+            PointPairList _pointPairList = new PointPairList();
+            List<Pair> list = new List<Pair>();
+            list = obj.WriteList();
+            foreach (Pair temp in list)
+            {
+                double x = temp.X;   
+                double y = temp.Y;
+                PointPair point_pair = new PointPair(x, y);
+                _pointPairList.Add(point_pair);
+            }
+            LineItem li = graphPane.AddCurve("Проекция неровностей", _pointPairList, Color.Red, SymbolType.None);
+
+            zedGraphControl1.AxisChange();
+
+        }
+        private void zedGraphControl1_Load(object sender, EventArgs e)
+        {
+ 
+        }
+    }
+}
---
+++ TryDraw/TryDraw/ReadData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZedGraph;
+
+namespace TryDraw
+{
+   class Pair
+    {
+
+        private double x;
+        public double X
+        {   
+            get {
+                return x;
+            }
+            set {
+                x = value;
+            }
+        }
+
+        private double y;
+        public double Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+
+                y = value;
+            }
+        }
+         
+        public Pair()
+        {
+            this.x = 0.0;
+            this.y = 0.0;
+        }
+        public Pair(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
+    class ReadData
+    {
+        
+        private string Get_data()
+        {
+            string path = "test.txt";
+            string line = "";
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            return line;
+        }
+
+        public List<Pair> WriteList()
+        {
+            List<Pair> list = new List<Pair>();
+            List<string[]> l = new List<string[]>();
+            string str = Get_data();
+            str = str.Trim(new char[] { '[', ']' });
+            string [] ss = str.Split(new string[] { "][" },StringSplitOptions.None);
+            foreach(string s in ss){
+               l.Add(s.Split(new char[] { ';' }));
+            }
+            foreach(string []s in l)
+            {
+                if (Convert.ToDouble(s[2]) > 8.0)
+                {
+                    list.Add(new Pair(Convert.ToDouble(s[0]), Convert.ToDouble(s[1])));          
+                }
+            }
+            return list;
+        }
+
+    }
+}
